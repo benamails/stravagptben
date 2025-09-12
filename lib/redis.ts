b6063c1cf6d6207a24fcd64b72a5777ffca41a0b
benamails <benamails+github@gmail.com>
2025-09-12 19:32:56 +0200

    Reset

--- lib/redis.ts
+++
@@ -1,9 +0,0 @@
-// lib/redis.ts - Version Upstash (recommandée)
-import { Redis } from '@upstash/redis';
-
-const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL!,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
-});
-
-export default redis;
